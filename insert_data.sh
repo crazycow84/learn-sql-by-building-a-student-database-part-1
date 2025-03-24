d93e5380d5a54a823265a8abf2ccb2b297ccd047
The Crazy Cow <thecrazycowexperience@gmail.com>
2025-03-24 09:36:06 +0000

    Save progress: 840.1

--- insert_data.sh
+++ insert_data.sh
@@ -23,7 +23,7 @@ then
     MAJOR_ID=$($PSQL "SELECT major_id FROM majors WHERE major='$MAJOR'")
   fi 
   #get course_id
-
+  COURSE_ID=$($PSQL "SELECT course_id FROM courses WHERE course='$COURSE'")
   #if not found
   
   #insert course
