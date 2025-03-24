994d016a86618f8d56928ab24ad3684bc395784e
The Crazy Cow <thecrazycowexperience@gmail.com>
2025-03-24 11:25:24 +0000

    Save progress: 1120.1

--- insert_data.sh
+++ insert_data.sh
@@ -6,7 +6,7 @@ PSQL="psql -X --username=freecodecamp --dbname=students --no-align --tuples-only
 
 echo $($PSQL "TRUNCATE students, majors, courses, majors_courses");
 
-cat courses_test.csv | while IFS="," read MAJOR COURSE
+cat courses.csv | while IFS="," read MAJOR COURSE
 do
 if [[ $MAJOR != major ]]
 then
