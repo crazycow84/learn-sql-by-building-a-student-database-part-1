8237962fe0609a93795a3d698b74170985469b91
The Crazy Cow <thecrazycowexperience@gmail.com>
2025-03-24 09:43:48 +0000

    Save progress: 853.1

--- insert_data.sh
+++ insert_data.sh
@@ -28,6 +28,7 @@ then
   if [[ -z $COURSE_ID ]]
   then
     #insert course
+    INSERT_COURSE_RESULT=$($PSQL "INSERT INTO courses(course) VALUES('$COURSE')")
     #get new course_id
   fi
   #insert into majors_courses
